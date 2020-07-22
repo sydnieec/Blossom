1ff3f2e39746ef982b01970f3cfec3a2f1315f29
Sydnie Chau <40672145+sydnieec@users.noreply.github.com>
2020-07-22 14:51:43 -0400

    Initial Commit

---
+++ Blossom/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Blossom
+//
+//  Created by Sydnie Chau on 2020-07-22.
+//  Copyright © 2020 Sydnie Chau. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
