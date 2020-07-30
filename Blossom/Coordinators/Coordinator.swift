81f285ab2d5012e1152079700a99c608ed7f2256
Sydnie Chau <40672145+sydnieec@users.noreply.github.com>
2020-07-30 16:28:20 -0400

    zoom on user location on map

--- Blossom/Coordinators/Coordinator.swift
+++ Blossom/Coordinators/Coordinator.swift
@@ -12,8 +12,23 @@ import MapKit
 final class Coordinator: NSObject, MKMapViewDelegate{
     
     var control : MapView
+    //how to zoom and move about the map
     init(_ control: MapView){
         self.control = control
         
+    }
+    
+    
+    //function to zoom onto user location upon opening screen
+    func mapView(_ mapView: MKMapView, didAdd views: [MKAnnotationView]) {
+        if let annotationView = views.first{
+            if let annotation = annotationView.annotation{
+                //checks if user location
+                if annotation is MKUserLocation{
+                    let region = MKCoordinateRegion(center: annotation.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+                    mapView.setRegion(region, animated: true)
+                }
+            }
+        }
     }
 }
